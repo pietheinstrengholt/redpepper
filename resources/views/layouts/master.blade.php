7bde380e88525fba37f9060ad1a883302987fcd0
Piethein Strengholt <piethein@xbmcfreak.nl>
2015-11-30 08:47:30 +0100

    add landing page

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -48,7 +48,7 @@
 	  <span class="icon-bar"></span>
 	  <span class="icon-bar"></span>
 	</button>
-	<a class="navbar-brand" href="<?php echo url('sections'); ?>">Home</a>
+	<a class="navbar-brand" href="<?php echo url('/'); ?>">Home</a>
 	</div>
 
 	<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
