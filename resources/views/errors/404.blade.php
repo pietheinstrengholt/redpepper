24b1e5b4a0b5e3f33e7ac30237cc045c71385e92
Piethein Strengholt <piethein@xbmcfreak.nl>
2015-11-30 16:58:15 +0100

    add proper error pages

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,14 +1,9 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>404 Error.</title>
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">404 error - content not found.</div>
-            </div>
-        </div>
-    </body>
-</html>
+<!-- /resources/views/errors/404.blade.php -->
+@extends('layouts.master')
+
+@section('content')
+	<h2>404 Error</h2>
+	<div class="title">Url not found, please contact the administrator.</div>
+@endsection
+
+@stop
