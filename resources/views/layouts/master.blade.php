68f53700869115d3f09fb3da3173ed1749d01e97
Piethein Strengholt <piethein@xbmcfreak.nl>
2015-12-01 15:56:44 +0100

    rename style.css to app.css

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -18,7 +18,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<link rel="stylesheet" href="{{ URL::asset('css/bootstrap.min.css') }}">
-	<link rel="stylesheet" href="{{ URL::asset('css/styles.css') }}">
+	<link rel="stylesheet" href="{{ URL::asset('css/app.css') }}">
 	<style type="text/css">
 		body {
 			padding-bottom: 40px;
