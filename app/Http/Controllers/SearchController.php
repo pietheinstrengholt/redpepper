821241382da6d034b65949f09786650d43924270
Piethein Strengholt <piethein@xbmcfreak.nl>
2015-12-03 13:51:08 +0100

    add advanced search

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -37,16 +37,52 @@ class SearchController extends Controller
 		if ($request->isMethod('post')) {
 			
 			if ($request->has('search')) {
+			
+				if ($request->has('sections') && $request->has('types')) {
 
-				return view('search.index', [
-					'rows' => TemplateRow::where('row_description', 'like', '%' . $request->input('search') . '%')->get(),
-					'columns' => TemplateColumn::where('column_description', 'like', '%' . $request->input('search') . '%')->get(),
-					'requirements' => Requirement::where('content', 'like', '%' . $request->input('search') . '%')->get(),
-					'technicals' => Technical::where('content', 'like', '%' . $request->input('search') . '%')->orWhere('description', 'like', '%' . $request->input('search') . '%')->get(),
-					'fields' => TemplateField::where('content', 'like', '%' . $request->input('search') . '%')->where('property', '<>', 'disabled')->get()
-				]);
+					//set typeslist
+					$typeslist = $request->input('types');
+					
+					//create empty array to fill with template_id's
+					$templatesArray = array();
+					
+					//retrieve template_id's for each section selected
+					foreach($request->input('sections') as $section) {
+						$templates = Template::where('section_id', $section)->get();
+						if (!empty($templates)) {
+							foreach($templates as $template) {
+								array_push($templatesArray, $template['id']);
+							}
+						}
+					}
+				
+					return view('search.index', [
+						'rows' => TemplateRow::where('row_description', 'like', '%' . $request->input('search') . '%')->whereIn('template_id', $templatesArray)->get(),
+						'columns' => TemplateColumn::where('column_description', 'like', '%' . $request->input('search') . '%')->whereIn('template_id', $templatesArray)->get(),
+						'requirements' => Requirement::where('content', 'like', '%' . $request->input('search') . '%')->whereIn('template_id', $templatesArray)->whereIn('content_type', $typeslist)->get(),
+						'technicals' => Technical::where('content', 'like', '%' . $request->input('search') . '%')->orWhere('description', 'like', '%' . $request->input('search') . '%')->whereIn('template_id', $templatesArray)->get(),
+						'fields' => TemplateField::where('content', 'like', '%' . $request->input('search') . '%')->where('property', '<>', 'disabled')->whereIn('template_id', $templatesArray)->whereIn('property', $typeslist)->get()
+					]);
+					
+				} else {
+					//no types and selections are selected
+					return view('search.index', [
+						'rows' => TemplateRow::where('row_description', 'like', '%' . $request->input('search') . '%')->get(),
+						'columns' => TemplateColumn::where('column_description', 'like', '%' . $request->input('search') . '%')->get(),
+						'requirements' => Requirement::where('content', 'like', '%' . $request->input('search') . '%')->get(),
+						'technicals' => Technical::where('content', 'like', '%' . $request->input('search') . '%')->orWhere('description', 'like', '%' . $request->input('search') . '%')->get(),
+						'fields' => TemplateField::where('content', 'like', '%' . $request->input('search') . '%')->where('property', '<>', 'disabled')->get()
+					]);
+				}
 			}
 		}
 	}
+	
+	public function advancedsearch() 
+	{
+		return view('search.advanced', [
+			'sections' => Section::where('visible', 'True')->orderBy('section_name', 'asc')->get()
+		]);
+	}	
 
 }
---
+++ resources/views/search/advanced.blade.php
@@ -0,0 +1,59 @@
+<!-- /resources/views/search/advanced.blade.php -->
+@extends('layouts.master')
+
+@section('content')
+    <h2>Advanced search</h2>
+	<h4>Use the search form below</h4>
+
+	{!! Form::open(array('action' => 'SearchController@search', 'id' => 'form')) !!}
+
+	<div class="panel panel-default">
+		<div class="panel-heading">Search</div>
+			<div class="panel-body">
+			
+			<div class="form-group">
+				<label for="caption">Search for:</label>
+				<input type="text" class="form-control" name="search" value="">
+			</div>	
+			
+			<div class="row">
+				<div class="col-md-1">
+				</div>
+				<div class="col-md-4 advanced-search">
+					<h4>Sections</h4>
+					@if ( $sections->count() )
+						@foreach( $sections as $section )
+							<div class="checkbox">
+							<label><input type="checkbox" name="sections[{{ $section->id }}]" value="{{ $section->id }}">{{ $section->section_name }}</label>
+							</div>
+						@endforeach
+					@endif
+				</div>
+				
+				
+				<div class="col-md-4 advanced-search">
+					<h4>Content types</h4>
+
+					<div class="checkbox"><label><input type="checkbox" name="types[1]" value="regulation">Regulation</label></div>	
+					<div class="checkbox"><label><input type="checkbox" name="types[2]" value="interpretation">Interpretations</label></div>
+					<div class="checkbox"><label><input type="checkbox" name="types[3]" value="property1">Reporting Business Rule</label></div>
+					<div class="checkbox"><label><input type="checkbox" name="types[4]" value="property2">Standard Operating Procedure</label></div>
+				
+				</div>
+				
+			</div>
+			
+
+		</div>			
+		<div class="col-md-1">
+		</div>
+		<button type="submit" class="btn btn-primary">Search</button>
+
+	</div>
+	
+	<input type="hidden" name="_token" value="{!! csrf_token() !!}">
+	{!! Form::close() !!}
+	
+@endsection
+
+@stop
