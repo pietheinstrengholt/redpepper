e6e3dae3faa6770e1e892865f80dac691a0d5ca1
Piethein Strengholt <piethein@xbmcfreak.nl>
2015-12-08 16:23:29 +0100

    add foreign key create script

---
+++ database/migrations/create_foreign.php
@@ -0,0 +1,89 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+		//on template_id
+        Schema::table('t_template_columns', function (Blueprint $table) {
+            $table->foreign('template_id')->references('id')->on('t_templates')->onDelete('cascade');
+        });
+        Schema::table('t_template_rows', function (Blueprint $table) {
+            $table->foreign('template_id')->references('id')->on('t_templates')->onDelete('cascade');
+        });
+        Schema::table('t_template_cells', function (Blueprint $table) {
+            $table->foreign('template_id')->references('id')->on('t_templates')->onDelete('cascade');
+        });
+        Schema::table('t_technical', function (Blueprint $table) {
+            $table->foreign('template_id')->references('id')->on('t_templates')->onDelete('cascade');
+        });
+        Schema::table('t_content', function (Blueprint $table) {
+            $table->foreign('template_id')->references('id')->on('t_templates')->onDelete('cascade');
+        });		
+        Schema::table('t_changes', function (Blueprint $table) {
+            $table->foreign('template_id')->references('id')->on('t_templates')->onDelete('cascade');
+        });
+        Schema::table('t_history_content', function (Blueprint $table) {
+            $table->foreign('template_id')->references('id')->on('t_templates')->onDelete('cascade');
+        });
+        Schema::table('t_history_technical', function (Blueprint $table) {
+            $table->foreign('template_id')->references('id')->on('t_templates')->onDelete('cascade');
+        });
+		
+		//on changerequest_id
+        Schema::table('t_changes_cells', function (Blueprint $table) {
+            $table->foreign('changerequest_id')->references('id')->on('t_changes')->onDelete('cascade');
+        });
+        Schema::table('t_changes_content', function (Blueprint $table) {
+            $table->foreign('changerequest_id')->references('id')->on('t_changes')->onDelete('cascade');
+        });
+        Schema::table('t_changes_technical', function (Blueprint $table) {
+            $table->foreign('changerequest_id')->references('id')->on('t_changes')->onDelete('cascade');
+        });
+        Schema::table('t_history_content', function (Blueprint $table) {
+            $table->foreign('changerequest_id')->references('id')->on('t_changes')->onDelete('cascade');
+        });
+		Schema::table('t_history_technical', function (Blueprint $table) {
+            $table->foreign('changerequest_id')->references('id')->on('t_changes')->onDelete('cascade');
+        });
+		
+		//on section_id
+		Schema::table('t_templates', function (Blueprint $table) {
+            $table->foreign('section_id')->references('id')->on('t_sections')->onDelete('cascade');
+        });
+		
+		//on username_id
+		Schema::table('t_usernames_rights', function (Blueprint $table) {
+            $table->foreign('username_id')->references('id')->on('t_username')->onDelete('cascade');
+        });
+		
+		//on source_id
+		Schema::table('t_technical', function (Blueprint $table) {
+            $table->foreign('source_id')->references('id')->on('t_t_technical_sources')->onDelete('cascade');
+        });
+
+		//on type_id
+		Schema::table('t_technical', function (Blueprint $table) {
+            $table->foreign('type_id')->references('id')->on('t_technical_types')->onDelete('cascade');
+        });
+		
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+		//
+    }
+}
