6c0082eb994dce60ffdd3cdf4f8319b7ea3b74da
Piethein Strengholt <piethein@strengholt-online.nl>
2015-12-14 19:48:07 +0100

    replace spaced with tabs for section index

--- resources/views/sections/index.blade.php
+++ resources/views/sections/index.blade.php
@@ -4,48 +4,48 @@
 @section('content')
 
 	<ul class="breadcrumb breadcrumb-section">
-	  <li><a href="{!! url('/'); !!}">Home</a></li>
-	  <li class="active">Sections</li>
+	<li><a href="{!! url('/'); !!}">Home</a></li>
+	<li class="active">Sections</li>
 	</ul>
 
 	<h2>Sections</h2>
 	<h4>Please make a selection of one of the following templates</h4>
 
 	@if ( !$sections->count() )
-	  No sections found in the database!<br><br>
+		No sections found in the database!<br><br>
 	@else
-	<table class="table section-table dialog table-striped" border="1">
+		<table class="table section-table dialog table-striped" border="1">
 
-	<tr class="success">
-	<td class="header">Name</td>
-	<td class="header">Description</td>
-	<td class="header" style="width: 120px;">Options</td>
-	</tr>
-
-	@foreach( $sections as $section )
-		@if ($section->visible == "False")
-			<tr class="notvisible">
-		@else
-			<tr>
-		@endif
-		<td><a href="{{ route('sections.show', $section->id) }}">{{ $section->section_name }}</a></td>
-		<td>{{ $section->section_description }}</td>
-		<td>
-		@if (!Auth::guest())
-			{!! Form::open(array('class' => 'form-inline', 'method' => 'DELETE', 'route' => array('sections.destroy', $section->id), 'onsubmit' => 'return confirm(\'Are you sure to delete this section?\')')) !!}
-			{!! link_to_route('sections.edit', 'Edit', array($section->id), array('class' => 'btn btn-info btn-xs')) !!}
-			{!! Form::submit('Delete', array('class' => 'btn btn-danger btn-xs', 'style' => 'margin-left:3px;')) !!}
-			{!! Form::close() !!}
-		@endif
-		</td>
+		<tr class="success">
+		<td class="header">Name</td>
+		<td class="header">Description</td>
+		<td class="header" style="width: 120px;">Options</td>
 		</tr>
-	@endforeach
 
-	</table>
+		@foreach( $sections as $section )
+			@if ($section->visible == "False")
+				<tr class="notvisible">
+			@else
+				<tr>
+			@endif
+			<td><a href="{{ route('sections.show', $section->id) }}">{{ $section->section_name }}</a></td>
+			<td>{{ $section->section_description }}</td>
+			<td>
+			@if (!Auth::guest())
+				{!! Form::open(array('class' => 'form-inline', 'method' => 'DELETE', 'route' => array('sections.destroy', $section->id), 'onsubmit' => 'return confirm(\'Are you sure to delete this section?\')')) !!}
+				{!! link_to_route('sections.edit', 'Edit', array($section->id), array('class' => 'btn btn-info btn-xs')) !!}
+				{!! Form::submit('Delete', array('class' => 'btn btn-danger btn-xs', 'style' => 'margin-left:3px;')) !!}
+				{!! Form::close() !!}
+			@endif
+			</td>
+			</tr>
+		@endforeach
+
+		</table>
 	@endif
 
 	<p>
-	  {!! link_to_route('sections.create', 'Create Section') !!}
+	{!! link_to_route('sections.create', 'Create Section') !!}
 	</p>
 
 @endsection
