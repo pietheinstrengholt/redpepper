d603f268c77832e219bab67a7355a741a4f1426f
Piethein Strengholt <piethein@strengholt-online.nl>
2015-12-14 11:01:12 +0100

    add date in overview with logs

--- resources/views/logs/index.blade.php
+++ resources/views/logs/index.blade.php
@@ -8,31 +8,33 @@
 	  <li class="active">User activities</li>
 	</ul>
 
-    <h2>Logs</h2>
+  <h2>Logs</h2>
 	<h4>The table below represents an overview of all user activities</h4>
- 
+
     @if ( !$logs->count() )
         No logs have been found in the database!<br><br>
     @else
-		<table class="table section-table dialog table-striped" border="1">
+			<table class="table section-table dialog table-striped" border="1">
 
-		<tr class="success">
-		<td class="header">Action type</td>
-		<td class="header">Description</td>
-		<td class="header" style="width: 20%;">User</td>
-		</tr>
-		
-		@foreach( $logs as $log )
-		<tr>
-		<td>{{ $log->content }}</a></td>
-		<td>{{ $log->description }}</a></td>
-		<td>{{ $log->user->firstname }} {{ $log->user->lastname }} ({{ $log->user->username }})</a></td>
-		</tr>
-		@endforeach
+			<tr class="success">
+			<td class="header">Action type</td>
+			<td class="header">Description</td>
+			<td class="header" style="width: 20%;">User</td>
+			<td class="header">Created</td>
+			</tr>
 
-		</table>
+			@foreach( $logs as $log )
+				<tr>
+				<td>{{ $log->content }}</a></td>
+				<td>{{ $log->description }}</a></td>
+				<td>{{ $log->user->firstname }} {{ $log->user->lastname }} ({{ $log->user->username }})</a></td>
+				<td>{{ $log->created_at }}</a></td>
+				</tr>
+			@endforeach
+
+			</table>
     @endif
-	
+
 @endsection
 
 @stop
