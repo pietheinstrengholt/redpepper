c45c5a3ac59e4e5e4aec49129ecc0ecb2e84b4e4
Piethein Strengholt <piethein@strengholt-online.nl>
2015-12-15 19:57:37 +0100

    remove and fix trailing spaces

--- app/Technical.php
+++ app/Technical.php
@@ -6,24 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Technical extends Model
 {
-    protected $fillable = ['template_id','source_id','type_id','content','row_code','column_code','content','description'];
-    protected $guarded = [];
+	protected $fillable = ['template_id','source_id','type_id','content','row_code','column_code','content','description'];
+	protected $guarded = [];
 	protected $table = 't_technical';
 
-    public function type()
-    {
+	public function type()
+	{
 		return $this->hasOne('App\TechnicalType', 'id', 'type_id');
-    }
+	}
 
-    public function source()
-    {
+	public function source()
+	{
 		return $this->hasOne('App\TechnicalSource', 'id', 'source_id');
-    }
+	}
 
-    public function template()
-    {
-        return $this->belongsTo('App\Template');
-    }
+	public function template()
+	{
+		return $this->belongsTo('App\Template');
+	}
 }
 
 ?>
