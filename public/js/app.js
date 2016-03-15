c514c45df5a9b8d0a9e8780d0201046754ab787b
Piethein Strengholt <piethein@strengholt-online.nl>
2016-03-15 10:37:32 +0100

    fix javascript error when adding new technical line

--- public/js/app.js
+++ public/js/app.js
@@ -174,13 +174,15 @@ jQuery(document).ready(function () {
 		$trNew.show();
 		$trNew.attr( "id", cloneCount );
 		$trLast.before($trNew);
+		
+		console.log(cloneCount);
 
 		//change name attribute and to increased clone count
-		$( 'tr#' + cloneCount + ' select#system_id.form-control').attr('name', 'technical[' + cloneCount + '][source_id]');
-		$( 'tr#' + cloneCount + ' select#type_id.form-control').attr('name', 'technical[' + cloneCount + '][type_id]');
-		$( 'tr#' + cloneCount + ' input#content.form-control').attr('name', 'technical[' + cloneCount + '][content]');
-		$( 'tr#' + cloneCount + ' input#description.form-control').attr('name', 'technical[' + cloneCount + '][description]');
-		$( 'tr#' + cloneCount + ' input#action').attr('name', 'technical[' + cloneCount + '][action]');
+		$( 'tr#' + cloneCount + ' td.source select.form-control').attr('name', 'technical[' + cloneCount + '][source_id]');
+		$( 'tr#' + cloneCount + ' td.type select.form-control').attr('name', 'technical[' + cloneCount + '][type_id]');
+		$( 'tr#' + cloneCount + ' td.content input.form-control').attr('name', 'technical[' + cloneCount + '][content]');
+		$( 'tr#' + cloneCount + ' td.description input.form-control').attr('name', 'technical[' + cloneCount + '][description]');
+		$( 'tr#' + cloneCount + ' td.action input').attr('name', 'technical[' + cloneCount + '][action]');
 
 	});
 	
