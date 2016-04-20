5ae93ed2c25f442002f12ab1bb92d3a62a5af54d
Piethein Strengholt <piethein@xbmcfreak.nl>
2016-04-20 16:46:18 +0200

    show breaks, hyperlinks in text, add missing descriptions

--- resources/views/templates/show.blade.php
+++ resources/views/templates/show.blade.php
@@ -10,10 +10,18 @@
 	  <li class="active">{{ $template->template_name }}</li>
 	</ul>
 
-    <h2>{{ $template->template_name }}</h2>
-	<h4>{{ $template->template_shortdesc }}</h4>
-	<h4>{{ $template->template_longdesc }}</h4>
-	<h5><a href="{!! url('/templatemanual/' . $template->id); !!}"><span class="glyphicon glyphicon-print" aria-hidden="true"></span> Print manual</a></h5>
+	<h2>{{ $template->template_name }}</h2>
+	<h4>{!! App\Helper::contentAdjust(nl2br(e($template->template_shortdesc))) !!}</h4>
+	<h4>{!! App\Helper::contentAdjust(nl2br(e($template->template_longdesc))) !!}</h4>
+	<h4>{!! App\Helper::contentAdjust(nl2br(e($template->frequency_description))) !!}</h4>
+	<h4>{!! App\Helper::contentAdjust(nl2br(e($template->reporting_dates_description))) !!}</h4>
+	<h4>{!! App\Helper::contentAdjust(nl2br(e($template->main_changes_description))) !!}</h4>
+	<h4>{!! App\Helper::contentAdjust(nl2br(e($template->links_other_temp_description))) !!}</h4>
+	<h4>{!! App\Helper::contentAdjust(nl2br(e($template->process_and_organisation_description))) !!}</h4>
+	
+	@if ($template->visible !== 'Limited')
+		<h5><a href="{!! url('/templatemanual/' . $template->id); !!}"><span class="glyphicon glyphicon-print" aria-hidden="true"></span> Print manual</a></h5>
+	@endif
 	
 	@if ( $descriptions->count() )
 		<div class="info-group" style="margin-bottom:10px;">
@@ -35,91 +43,94 @@
 		</table><br></div></div></div>
 	@endif
 	
-	<!-- Hidden div to carry template id -->
-	<div class="templateId" id="{{ $template->id }}" style="display: none;"></div>
+	@if ($template->visible !== 'Limited')
+		<!-- Hidden div to carry template id -->
+		<div class="templateId" id="{{ $template->id }}" style="display: none;"></div>
 
-	@if ( !$template->columns->count() || !$template->rows->count() )
-		Error: This template has no columns or no rows.
-	@else
-		<table class="table table-bordered template" border="1">
+		@if ( !$template->columns->count() || !$template->rows->count() )
+			Error: This template has no columns or no rows.
+		@else
+			<table class="table table-bordered template" border="1">
 
-		<!-- Table header with column names -->
-		<thead>
-		<tr class="success">
+			<!-- Table header with column names -->
+			<thead>
+			<tr class="success">
 
-		<td class="header content">Row#</td>
-		<td class="header">Row description</td>
-		<td class="header">Reference</td>
+			<td class="header content">Row#</td>
+			<td class="header">Row description</td>
+			<td class="header">Reference</td>
 
-		@foreach( $template->columns as $column )
-			<td class="content header" id="$column->column_code">
-			{{ $column->column_description }}
-			</td>
-		@endforeach
-		</tr>
-
-		<!-- Table header with column nums -->
-
-		<tr class="header2">
-
-		<td></td>
-		<td></td>
-		<td></td>
-
-		@foreach( $template->columns as $column )
-			<td class="column">
-			{{ $column->column_code }}
-			</td>
-		@endforeach
-
-		</tr>
-		</thead>
-
-		<!-- Table content with row information -->
-		<tbody>
-		@foreach( $template->rows as $row )
-
-			<tr>
-			<td class="desc">{{ $row->row_code }}</td>
-			<td class="desc property{{ $row->row_property }}">{{ $row->row_description }}</td>
-			<td class="desc property{{ $row->reference }}">{{ $row->row_reference }}</td>
-			<!-- Table cell information, column and row combination -->
 			@foreach( $template->columns as $column )
-
-				<!-- Create a new variable, column and row combination -->
-				{{--*/ $field = 'column' . $column->column_code . '-row' . $row->row_code /*--}}
-
-				@if (array_key_exists($field, $disabledFields))
-					<td title="{{ $column->column_description }} - {{ $row->row_description }}" class="disabled" id="{{ $field }}"></td>
-				@else
-					@if (strpos($field,$searchvalue) !== false)
-						<td title="{{ $column->column_description }} - {{ $row->row_description }}" class="tablecell highlight" id="{{ $field }}">
-							@if (array_key_exists($field, $propertyFields))
-								{{ $propertyFields[$field] }}
-							@endif
-						</td>
-					@else
-						<td title="{{ $column->column_description }} - {{ $row->row_description }}" class="tablecell" id="{{ $field }}">
-							@if (array_key_exists($field, $propertyFields))
-								{{ $propertyFields[$field] }}
-							@endif
-						</td>
-					@endif
-				@endif
-
+				<td class="content header" id="$column->column_code">
+				{{ $column->column_description }}
+				</td>
 			@endforeach
 			</tr>
 
-		@endforeach
-		</tbody>
+			<!-- Table header with column nums -->
 
-		</table>
+			<tr class="header2">
 
-    @endif
+			<td></td>
+			<td></td>
+			<td></td>
 
-	<p>
-	<a href="{!! url('/sections/' . $template->section->id); !!}">Back to Sections</a>
-	</p>
+			@foreach( $template->columns as $column )
+				<td class="column">
+				{{ $column->column_code }}
+				</td>
+			@endforeach
+
+			</tr>
+			</thead>
+
+			<!-- Table content with row information -->
+			<tbody>
+			@foreach( $template->rows as $row )
+
+				<tr>
+				<td class="desc">{{ $row->row_code }}</td>
+				<td class="desc property{{ $row->row_property }}">{{ $row->row_description }}</td>
+				<td class="desc property{{ $row->reference }}">{{ $row->row_reference }}</td>
+				<!-- Table cell information, column and row combination -->
+				@foreach( $template->columns as $column )
+
+					<!-- Create a new variable, column and row combination -->
+					{{--*/ $field = 'column' . $column->column_code . '-row' . $row->row_code /*--}}
+
+					@if (array_key_exists($field, $disabledFields))
+						<td title="{{ $column->column_description }} - {{ $row->row_description }}" class="disabled" id="{{ $field }}"></td>
+					@else
+						@if (strpos($field,$searchvalue) !== false)
+							<td title="{{ $column->column_description }} - {{ $row->row_description }}" class="tablecell highlight" id="{{ $field }}">
+								@if (array_key_exists($field, $propertyFields))
+									{{ $propertyFields[$field] }}
+								@endif
+							</td>
+						@else
+							<td title="{{ $column->column_description }} - {{ $row->row_description }}" class="tablecell" id="{{ $field }}">
+								@if (array_key_exists($field, $propertyFields))
+									{{ $propertyFields[$field] }}
+								@endif
+							</td>
+						@endif
+					@endif
+
+				@endforeach
+				</tr>
+
+			@endforeach
+			</tbody>
+
+			</table>
+
+		@endif
+
+		<p>
+		<a href="{!! url('/sections/' . $template->section->id); !!}">Back to Sections</a>
+		</p>
+
+	@endif
 
 @include('templates.modal')
 
