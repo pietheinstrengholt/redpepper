e9140a14169bc27b2173722d4c9364c1f0909a10
Piethein Strengholt <piethein@strengholt-online.nl>
2016-05-11 19:52:17 +0200

    update deploy.sh - use reset origin/master instead of HEAD

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,10 @@
+#!/bin/bash
+cd html
 php artisan down
+git status
 git clean -n -d
-git clean -f -d -e deploy.sh
-git reset --hard HEAD
+git clean -f -d
+git reset --hard origin/master
 git pull
 git log --pretty="%h" -n1 HEAD > version
 composer install
@@ -14,4 +17,3 @@ chown www-data:www-data .env -R
 chown www-data:www-data public/.htaccess -R
 chmod -R 777 storage/
 php artisan up
-chmod +x ./deploy.sh
