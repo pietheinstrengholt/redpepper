a315103f791c5850698ee3ec7d38f77015f02556
Piethein Strengholt <piethein@strengholt-online.nl>
2016-05-31 17:33:50 +0200

    add valid_elements and style_formats for templates

--- resources/views/templates/edit.blade.php
+++ resources/views/templates/edit.blade.php
@@ -7,6 +7,7 @@
 
 	<script>tinymce.init({ 	
 		selector:'textarea#template_shortdesc',
+		valid_elements: "p[style],h1,h2,h3,h4,h5,a[href|target],strong/b,i/em,br,table,tbody,thead,tr,td,ul,ol,li,img[src]",
 		height: 200,
 		plugins: [
 			'link image imageupload'
@@ -21,9 +22,17 @@
 
 	<script>tinymce.init({ 	
 		selector:'textarea#template_longdesc',
+		valid_elements: "p[style],h1,h2,h3,h4,h5,a[href|target],strong/b,i/em,br,table,tbody,thead,tr,td,ul,ol,li,img[src]",
 		height: 400,
 		plugins: [
 			'link image imageupload table'
+		],
+		style_formats : [
+			{title : 'Heading 1', inline : 'h1', classes : 'h1'},
+			{title : 'Heading 2', inline : 'h2', classes : 'h2'},
+			{title : 'Heading 3', inline : 'h3', classes : 'h3'},
+			{title : 'Heading 4', inline : 'h4', classes : 'h4'},
+			{title : 'Heading 5', inline : 'h5', classes : 'h5'},
 		],
 		menubar: '',
 		toolbar: 'undo redo | alignleft aligncenter alignright | styleselect | bold italic | outdent indent | link | imageupload | bullist numlist | table ',
