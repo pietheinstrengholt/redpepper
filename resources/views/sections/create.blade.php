b634f6d9653511eed8edefdb73a6f55947ed9f2d
Piethein Strengholt <piethein@strengholt-online.nl>
2016-05-31 16:14:04 +0200

    add tinymce when creating section

--- resources/views/sections/create.blade.php
+++ resources/views/sections/create.blade.php
@@ -2,6 +2,41 @@
 @extends('layouts.master')
 
 @section('content')
+
+	<script src="{{ URL::asset('js/tinymce/tinymce.min.js') }}"></script>
+
+	<script>tinymce.init({ 	
+		selector:'textarea#section_description',
+		height: 200,
+		plugins: [
+			'link image'
+		],
+		menubar: '',
+		toolbar: 'undo redo | bold italic | link | bullist numlist',
+		body_class: 'form-control',
+		statusbar: false,
+		content_style: "p {margin-top: -4px;} ol,ul,p {color: #2c3e50; font-size: 15px;}"
+	});</script>
+
+	<script>tinymce.init({ 	
+		selector:'textarea#section_longdesc',
+		height: 300,
+		plugins: [
+			'link image'
+		],
+		menubar: '',
+		toolbar: 'undo redo | alignleft aligncenter alignright | bold italic | link | bullist numlist',
+		body_class: 'form-control',
+		statusbar: false,
+		content_style: "p {margin-top: -4px;} ol,ul,p {color: #2c3e50; font-size: 15px;}"
+	});</script>
+
+	<ul class="breadcrumb breadcrumb-section">
+	<li><a href="{!! url('/'); !!}">Home</a></li>
+	<li><a href="{!! url('/sections/'); !!}">Sections</a></li>
+	<li class="active">Create Section</li>
+	</ul>
+
 	<h2>Create Section</h2>
 
 	@if (count($errors) > 0)
