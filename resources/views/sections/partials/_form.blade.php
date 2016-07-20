e36f22fdfc143531880cbdfd22637f775b514971
Piethein Strengholt <piethein@strengholt-online.nl>
2016-07-20 13:43:21 +0200

    make edit section description more rich

--- resources/views/sections/partials/_form.blade.php
+++ resources/views/sections/partials/_form.blade.php
@@ -18,21 +18,21 @@
 
 	<div class="form-group">
 		{!! Form::label('section_longdesc', 'Detailed description:', array('class' => 'col-sm-3 control-label')) !!}
-		<div class="col-sm-6">
+		<div class="col-sm-8">
 		{!! Form::textarea('section_longdesc', null, ['class' => 'form-control', 'rows' => '7']) !!}
 		</div>
 	</div>
 
 	<div class="form-group">
 		{!! Form::label('subject_id', 'Section group:', array('class' => 'col-sm-3 control-label')) !!}
-		<div class="col-sm-6">
+		<div class="col-sm-4">
 		{!! Form::select('subject_id', $subjects->lists('subject_name', 'id'), $subject_id, ['id' => 'subject_id', 'class' => 'form-control']) !!}
 		</div>
 	</div>
 
 	<div class="form-group">
 		{!! Form::label('visible', 'Visible:', array('class' => 'col-sm-3 control-label')) !!}
-		<div class="col-sm-6">
+		<div class="col-sm-4">
 		{!! Form::select('visible', ['True' => 'Yes, all users can see this section', 'False' => 'No, only visible for (super)admin, builder users'], $section->visible, ['id' => 'visible', 'class' => 'form-control']) !!}
 		</div>
 	</div>
--- resources/views/tinymce/section.blade.php
+++ resources/views/tinymce/section.blade.php
@@ -3,17 +3,27 @@
 
 <script>tinymce.init({ 	
 	selector:'textarea#section_longdesc',
-	valid_elements: "p,a[href|target],strong/b,i/em,u,br,ul,ol,li",
+valid_elements: "p,h1,h2,h3,h4,h5,a[href|target],strong/b,i/em,u,br,table[*],tbody[*],thead[*],tr[*],td[*],ul,ol,li,img[src|height|width]",
 	formats : {
 		underline : {inline : 'u', exact : true}
 	},
 	height: 300,
 	plugins: [
-		'link image'
+		'link image table nonbreaking searchreplace'
+	],
+	style_formats : [
+		{title : 'Heading 1', block : 'h1'},
+		{title : 'Heading 2', block : 'h2'},
+		{title : 'Heading 3', block : 'h3'},
+		{title : 'Heading 4', block : 'h4'},
+		{title : 'Heading 5', block : 'h5'},
+		{title : 'Paragraph', block : 'p'},
 	],
 	menubar: '',
-	toolbar: 'undo redo | alignleft aligncenter alignright | bold italic underline | link | bullist numlist',
+	toolbar: 'undo redo | alignleft aligncenter alignright | styleselect | bold italic underline | outdent indent | link | bullist numlist | table | nonbreaking  | searchreplace',
+	relative_urls: false,
 	statusbar: false,
+	style_format_merge: true,
 	content_css: ["{!! URL::asset('css') . '/' . App\Helper::setting('css_style') !!}"],
 	content_style: "body {margin: 10px ! important; }",
 });</script>
