ca7d885022a3f4bd3ee8ed2b60dd433b74a2c6e1
Piethein Strengholt <piethein@strengholt-online.nl>
2016-08-21 08:37:26 +0200

    completely rewrite the way technical import csv works - do not use Excel Maatwebsite anymore but native php functions instead

--- app/Http/Controllers/CSVController.php
+++ app/Http/Controllers/CSVController.php
@@ -16,10 +16,10 @@ use Event;
 use App\Events\SectionUpdated;
 use Gate;
 use Illuminate\Http\Request;
-use Maatwebsite\Excel\Facades\Excel;
 use Redirect;
 use Session;
 use Validator;
+use Input;
 
 class CSVController extends Controller
 {
@@ -36,114 +36,147 @@ class CSVController extends Controller
 			'formname' => 'required'
 		]);
 
+		//validate if message type is post
 		if ($request->isMethod('post')) {
 
-			if ($request->hasFile('csv')) {
-				if ($request->file('csv')->isValid()) {
+			//check if file is valid
+			if (Input::file('csv')->isValid()) {
 
-					$file = array('csv' => $request->file('csv'));
+				//get file content
+				$file = Input::file('csv');
+				$content = file_get_contents($file->getPathName());
 
-					//Abort if the file extension is not CSV
-					if ($request->file('csv')->getClientOriginalExtension() <> 'csv') {
-						abort(403, 'Unable to import CSV file, filetype is no CSV');
+				//Abort if the file extension is not CSV
+				if (Input::file('csv')->getClientOriginalExtension() <> 'csv') {
+					abort(403, 'Unable to import CSV file, filetype is no CSV');
+				}
+
+				//parse CSV string
+				$lines = explode(PHP_EOL, $content);
+				$array = array();
+				foreach ($lines as $line) {
+				    $array[] = str_getcsv($line);
+				}
+
+				$section = Section::findOrFail($request->input('section_id'));
+				$templates = Template::where('section_id', $request->input('section_id'))->get();
+				$sources =  TechnicalSource::all();
+				$types =  TechnicalType::all();
+
+				//Abort when there are no templates
+				if (empty($templates)) {
+					abort(403, 'Unable to import CSV file, no templates found for this section');
+				}
+
+				//Abort when there are no sources
+				if (empty($sources)) {
+					abort(403, 'Unable to import CSV file, no sources are found in the database');
+				}
+
+				//Abort when there are no types
+				if (empty($types)) {
+					abort(403, 'Unable to import CSV file, no types are found in the database');
+				}
+
+				//Create empty array for template id validation
+				$templatesArray = array();
+				foreach ($templates as $template) {
+					array_push($templatesArray,$template->id);
+				}
+
+				//Create empty array for template id validation
+				$sourcesArray = array();
+				foreach ($sources as $source) {
+					array_push($sourcesArray,$source->id);
+				}
+
+				//Create empty array for template id validation
+				$typesArray = array();
+				foreach ($types as $type) {
+					array_push($typesArray,$type->id);
+				}
+
+				foreach ($array as $key => $record) {
+					//explode text lines
+					$exploded = explode(';', $record[0]);
+
+					//first line is the header, validate csv structure, abort when incorrect items are found
+					if ($key == 0) {
+						if ($exploded[0] != "template_id") {
+							abort(403, 'Header does not contain template_id');
+						}
+						if ($exploded[1] != "row_code") {
+							abort(403, 'Header does not contain row_code');
+						}
+						if ($exploded[2] != "column_code") {
+							abort(403, 'Header does not contain column_code');
+						}
+						if ($exploded[3] != "source_id") {
+							abort(403, 'Header does not contain source_id');
+						}
+						if ($exploded[4] != "type_id") {
+							abort(403, 'Header does not contain type_id');
+						}
+						if ($exploded[5] != "content") {
+							abort(403, 'Header does not contain content');
+						}
+						if ($exploded[6] != "description") {
+							abort(403, 'Header does not contain description');
+						}
 					}
 
-					//Use Excel functionality to parse CSV file
-					Excel::load($request->file('csv'), function ($reader) use ($request) {
-
-						//Create array from csv content
-						$csvarray = $reader->toArray();
-
-						//Abort when only a header is used and no content can be found
-						if (empty($csvarray)) {
-							abort(403, 'Unable to import CSV file, no content is found');
+					//validate content. 6 values used is the minimum. The description might be empty.
+					//TODO: validate in CSV for existing row_code, column_code in template
+					if ($key > 0 && count($exploded) > 5) {
+						if (!in_array($exploded[0], $templatesArray)) {
+							abort(403, 'There have been incorrect template_id found that does\'t belong to selected section');
 						}
-
-						//import rows function
-						if ($request->input('formname') == "importtech" && $request->has('section_id')) {
-
-							$section = Section::findOrFail($request->input('section_id'));
-							$templates = Template::where('section_id', $request->input('section_id'))->get();
-							$sources =  TechnicalSource::all();
-							$types =  TechnicalType::all();
-
-							//Abort when there are no templates
-							if (empty($templates)) {
-								abort(403, 'Unable to import CSV file, no templates found for this section');
-							}
-
-							//Abort when there are no sources
-							if (empty($sources)) {
-								abort(403, 'Unable to import CSV file, no sources are found in the database');
-							}
-
-							//Abort when there are no types
-							if (empty($types)) {
-								abort(403, 'Unable to import CSV file, no types are found in the database');
-							}
-
-							//Create empty array for template id validation
-							$templatesArray = array();
-							foreach ($templates as $template) {
-								array_push($templatesArray,$template->id);
-							}
-
-							//Create empty array for template id validation
-							$sourcesArray = array();
-							foreach ($sources as $source) {
-								array_push($sourcesArray,$source->id);
-							}
-
-							//Create empty array for template id validation
-							$typesArray = array();
-							foreach ($types as $type) {
-								array_push($typesArray,$type->id);
-							}
-
-							//validate before import CSV content
-							//TODO: validate in CSV for existing row_code, column_code in template
-							foreach ($csvarray as $csv) {
-								if (!(array_key_exists('template_id', $csv) && array_key_exists('row_code', $csv) && array_key_exists('column_code', $csv) && array_key_exists('source_id', $csv) && array_key_exists('type_id', $csv)  && array_key_exists('content', $csv)  && array_key_exists('description', $csv))) {
-									abort(403, 'Unable to import CSV file, header is incorrect');
-								}
-								if (!in_array($csv['template_id'], $templatesArray)) {
-									abort(403, 'Template id used in CSV file does\'t belong to selected section');
-								}
-								if (!in_array($csv['source_id'], $sourcesArray)) {
-									abort(403, 'source id used in CSV file can\'t be matched with any source id in the database');
-								}
-								if (!in_array($csv['type_id'], $typesArray)) {
-									abort(403, 'type id used in CSV file can\'t be matched with any type id in the database');
-								}
-							}
-
-							//remove existing content if count is zero
-							foreach ($templates as $template) {
-								Technical::where('template_id', $template->id)->delete();
-							}
-
-							//Import CSV content
-							foreach ($csvarray as $csv) {
-								$technical = new Technical;
-								$technical->template_id = $csv['template_id'];
-								$technical->row_code = $csv['row_code'];
-								$technical->column_code = $csv['column_code'];
-								$technical->source_id = $csv['source_id'];
-								$technical->type_id = $csv['type_id'];
-								$technical->content = $csv['content'];
-								$technical->description = $csv['description'];
-								$technical->created_by = Auth::user()->id;
-								$technical->save();
-							}
-
-							//log Event
-							Event::fire(new SectionUpdated($section));
+						if (empty($exploded[1])) {
+							abort(403, 'There have an empty row_code used.');
 						}
-					});
+						if (empty($exploded[2])) {
+							abort(403, 'There have an empty column_code used.');
+						}
+						if (!in_array($exploded[3], $sourcesArray)) {
+							abort(403, 'There have been incorrect source_id\'s found that can\'t be matched with any source_id in the database');
+						}
+						if (!in_array($exploded[4], $typesArray)) {
+							abort(403, 'There have been incorrect type_id\'s found that can\'t be matched with any type_id in the database');
+						}
+					}
 				}
-			}
 
-			return Redirect::to('/sections')->with('message', 'CSV successfully imported to the database.');
+				//all validations have been passed, it's time to delete all existing content
+				foreach ($templates as $template) {
+					Technical::where('template_id', $template->id)->delete();
+				}
+
+				//add new content to database
+				foreach ($array as $key => $record) {
+					//explode text lines
+					$exploded = explode(';', $record[0]);
+
+					if ($key > 0 && count($exploded) > 5) {
+						$technical = new Technical;
+						$technical->template_id = $exploded[0];
+						$technical->row_code = $exploded[1];
+						$technical->column_code = $exploded[2];
+						$technical->source_id = $exploded[3];
+						$technical->type_id = $exploded[4];
+						$technical->content = $exploded[5];
+						if (!empty($exploded[6])) {
+							$technical->description = $exploded[6];
+						}
+						$technical->created_by = Auth::user()->id;
+						$technical->save();
+					}
+				}
+
+				//log Event
+				Event::fire(new SectionUpdated($section));
+
+				return Redirect::to('/sections')->with('message', 'CSV successfully imported to the database.');
+			}
 		}
 	}
 
