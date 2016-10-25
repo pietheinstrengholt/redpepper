297cf54818007ac943e2b062b1666f2e199e28e6
Piethein Strengholt <piethein@xbmcfreak.nl>
2016-10-25 15:26:02 +0200

    fix not getting all subjects

--- app/Http/ViewComposers/SubjectBlockComposer.php
+++ app/Http/ViewComposers/SubjectBlockComposer.php
@@ -12,9 +12,13 @@ class SubjectBlockComposer {
 	public function compose(View $view) {
 		//only non guests will see the hidden templates
 		if (Gate::allows('see-nonvisible-content')) {
-			$view->with('subjects', Subject::orderBy('subject_order', 'asc')->orderBy('subject_name', 'asc')->where('parent_id', null)->get());
+			$view->with('subjects', Subject::where('parent_id', null)->orWhere(function ($query) {
+				$query->where('parent_id', 0);
+			})->orderBy('subject_order', 'asc')->orderBy('subject_name', 'asc')->get());
 		} else {
-			$view->with('subjects', Subject::orderBy('subject_order', 'asc')->orderBy('subject_name', 'asc')->where('visible', '<>' , 'False')->where('parent_id', null)->get());
+			$view->with('subjects', Subject::where('visible', '<>' , 'False')->where('parent_id', null)->orWhere(function ($query) {
+				$query->where('visible', '<>' , 'False')->where('parent_id', 0);
+			})->orderBy('subject_order', 'asc')->orderBy('subject_name', 'asc')->get());
 		}
 	}
 }
