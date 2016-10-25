3710535af6842e7b97743e364dd23085fe6eaaf5
Piethein Strengholt <piethein@xbmcfreak.nl>
2016-10-25 15:27:28 +0200

    fix getting all subjects

--- app/Http/ViewComposers/SubjectComposer.php
+++ app/Http/ViewComposers/SubjectComposer.php
@@ -12,9 +12,13 @@ class SubjectComposer {
 	public function compose(View $view) {
 		//only non guests will see the hidden templates
 		if (Auth::guest()) {
-			$view->with('subjects', Subject::orderBy('subject_name', 'asc')->where('visible', '<>' , 'False')->where('parent_id', null)->get());
+			$view->with('subjects', Subject::where('visible', '<>' , 'False')->where('parent_id', null)->orWhere(function ($query) {
+				$query->where('visible', '<>' , 'False')->where('parent_id', 0);
+			})->orderBy('subject_order', 'asc')->orderBy('subject_name', 'asc')->get());
 		} else {
-			$view->with('subjects', Subject::orderBy('subject_name', 'asc')->where('parent_id', null)->get());
+			$view->with('subjects', Subject::where('parent_id', null)->orWhere(function ($query) {
+				$query->where('parent_id', 0);
+			})->orderBy('subject_order', 'asc')->orderBy('subject_name', 'asc')->get());
 		}
 	}
 }
