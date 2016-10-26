ad7491e059259872170c58a98540625f0622df93
Piethein Strengholt <piethein@strengholt-online.nl>
2016-10-26 17:17:36 +0200

    fix route in changerequests

--- resources/views/changerequests/index.blade.php
+++ resources/views/changerequests/index.blade.php
@@ -34,7 +34,7 @@
 			@if ( $changerequest->status == "pending" )
 				<tr>
 					<td><a href="{!! url('changerequests/') . "/" . $changerequest->id . "/edit" !!}">{{ $changerequest->id }}</a></td>
-					<td><a href="{!! url('sections/' . $changerequest->template->section_id . '/templates/' . $changerequest->template_id . '?row=' . $changerequest->row_code . '&column=' . $changerequest->column_code); !!}">{{ $changerequest->template->template_name }}</a></td>
+					<td><a href="{!! url('subjects/' . $changerequest->template->section->subject->id . '/sections/' . $changerequest->template->section->id . '/templates/' . $changerequest->template->id . '?row=' . $changerequest->row_code . '&column=' . $changerequest->column_code); !!}">{{ $changerequest->template->template_name }}</a></td>
 					<td>{{ $changerequest->creator->username }}</td>
 					<td></td>
 					<td>{{ $changerequest->template->section->section_name }}</td>
