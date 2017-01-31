a8eff806e2b86af6a4d8fe4395321cabbddd3282
Piethein Strengholt <piethein@strengholt-online.nl>
2017-01-31 11:21:05 +0100

    fix html behaviour cell pop-up

--- resources/views/changerequests/index.blade.php
+++ resources/views/changerequests/index.blade.php
@@ -54,7 +54,7 @@
 			@else
 				<tr style="background-color:#F5F5F5;">
 					<td><a href="{!! url('changerequests/') . "/" . $changerequest->id . "/edit" !!}">{{ $changerequest->id }}</a></td>
-					<td><a href="{!! url('sections/' . $changerequest->template->section_id . '/templates/' . $changerequest->template_id . '?row=' . $changerequest->row_code . '&column=' . $changerequest->column_code); !!}">{{ $changerequest->template->template_name }}</a></td>
+					<td><a href="{!! url('subjects/' . $changerequest->template->section->subject->id . '/sections/' . $changerequest->template->section_id . '/templates/' . $changerequest->template_id . '?row=' . $changerequest->row_code . '&column=' . $changerequest->column_code); !!}">{{ $changerequest->template->template_name }}</a></td>
 					<td>{{ $changerequest->creator->username }}</td>
 					<td></td>
 					<td>{{ $changerequest->template->section->section_name }}</td>
